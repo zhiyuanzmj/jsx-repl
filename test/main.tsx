1a18416301bd91838263da0bc4e63a90a5f5802c
zhiyuanzmj <260480378@qq.com>
2026-02-18 15:25:53 +0800

    chore: remove test

--- test/main.tsx
+++ test/main.tsx
@@ -8,7 +8,6 @@ window.process = { env: {}, cwd: () => '/' }
 
 const App = defineVaporComponent(() => {
   const src = $useRoutePath('vue-jsx')
-  window.NITRO_CLIENT_ID = import.meta.env.NITRO_CLIENT_ID
   return <Repl v-model={src} theme="dark" />
 })
 
