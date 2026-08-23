a0e5665cdcd92d37bb27690ff6bbb9862398902e
zhiyuanzmj <260480378@qq.com>
2026-08-24 01:26:18 +0800

    chore: update deps

--- src/monaco/env.ts
+++ src/monaco/env.ts
@@ -51,9 +51,9 @@ export async function reloadLanguageTools(store: Store) {
   disposeVue?.()
 
   let dependencies: Record<string, string> = {
-    '@vue/runtime-vapor': '3.6.0-rc.3',
-    '@vue/runtime-core': '3.6.0-rc.3',
-    '@vue-jsx-vapor/runtime': '3.2.21',
+    '@vue/runtime-vapor': '3.6.0-rc.5',
+    '@vue/runtime-core': '3.6.0-rc.5',
+    '@vue-jsx-vapor/runtime': '3.2.22',
     ...store.dependencies,
   }
 
